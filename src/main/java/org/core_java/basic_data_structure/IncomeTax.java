8522f3c36a4d54bdb2f5a317e05d4bf359c0f093
Atul Jaware Patil <atuljaware.com>
2023-07-24 16:08:10 +0530

    IncomeTax Added

---
+++ src/main/java/org/core_java/basic_data_structure/IncomeTax.java
@@ -0,0 +1,23 @@
+package org.core_java.basic_data_structure;
+
+import java.util.Scanner;
+
+public class IncomeTax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter CTC : ");
+        long ctc=sc.nextLong();
+        double tax=0;
+
+        if(ctc <= 180000)
+            tax=0;
+       else if(ctc >181000 && ctc <= 300000)
+            tax=(ctc*0.1);
+       else if(ctc >300000 && ctc <= 500000)
+            tax=(ctc*0.2);
+       else if(ctc >500000 && ctc <= 1000000)
+            tax=(ctc*0.3);
+
+        System.out.println("Tax : "+ tax);
+    }
+}
