76e667460d507bb34489e25b59c3f4a50d7891b6
Atul Jaware Patil <atuljaware.com>
2023-07-28 18:37:14 +0530

    Assignement on String Added

--- src/main/java/org/core_java/stringclass_stringbuffer/Q1_StringLength.java
+++ src/main/java/org/core_java/stringclass_stringbuffer/Q1_StringLength.java
@@ -3,7 +3,7 @@ package org.core_java.stringclass_stringbuffer;
 public class Q1_StringLength {
     public static void main(String[] args) {
         String str="Hello World";
-        int length=str.length();
-        System.out.println("Length of str = "+length);
+        int len=str.length();
+        System.out.println("Length of str = "+len);
     }
 }
--- src/main/java/org/core_java/stringclass_stringbuffer/Q2_JoinString.java
+++ src/main/java/org/core_java/stringclass_stringbuffer/Q2_JoinString.java
@@ -2,10 +2,10 @@ package org.core_java.stringclass_stringbuffer;
 
 public class Q2_JoinString {
     public static void main(String[] args) {
-        String str= "Hello,";
+        String str1= "Hello,";
         String str2="How are you ?";
 
-        String join= str.concat(str2);
+        String join= str1.concat(str2);
         System.out.println(join);
     }
 }
